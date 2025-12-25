f8cfab29b7f4c7532d05e5fae89ad8f18afa854a
Dhiraj <dhirajtarte5@gmail.com>
2025-12-26 01:33:13 +0530

    My initial froject set up

---
+++ JavaProject1/src/com/javabasics/advance/TestClass2.java
@@ -0,0 +1,7 @@
+package com.javabasics.advance;
+
+public class TestClass2 {
+    public static void main(String[] args) {
+        System.out.println("TestClass2");
+    }
+}
---
+++ JavaProject1/src/com/javabasics/basics/TestClass1.java
@@ -0,0 +1,7 @@
+package com.javabasics.basics;
+
+public class TestClass1 {
+    public static void main(String[] args) {
+        System.out.println("TestClass1");
+    }
+}
